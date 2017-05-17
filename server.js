530b72b7f1feb5520886f62078c2a82b51c43d87
Caleb McDonnell <caleb@mcdonnell.geek.nz>
2017-05-18 09:28:35 +1200

    Add projects page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about', (req, res) => {
     pageTitle: "About Page",
   });
 })
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "Projects Page",
+  });
+})
+
 app.get('/bad', (req, res) => {
   res.send({errorMessage: "Unable to handle request."})
 })
